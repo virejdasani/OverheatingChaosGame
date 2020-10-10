34a581eba6ae42f4622fe2f5f46cf8ae2f88e76e
virejdasani <dasanivirej@gmail.com>
2020-10-10 16:18:31 +0530

    Compiled first viable version: 1.0.apk

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeManager : MonoBehaviour
+{
+
+    public static TextMeshProUGUI text;
+    public static float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.000f;
+        text = GetComponent<TextMeshProUGUI>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "time: " + time.ToString();
+        time ++;
+    }
+}
